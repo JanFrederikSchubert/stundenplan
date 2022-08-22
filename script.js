ec194e0b9f09e5727f3283275c8530e2c40fad81
Lastand Development <99511621+LastandDevelopment@users.noreply.github.com>
2022-08-22 21:38:14 +0200

    Finished Basic Version

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-var root = document.querySelector(":root");
+var root = document.documentElement;
 
 var colorPicker = document.getElementById("colorPicker");
-var colorDisplay = document.getElementById("colorDisplay");
+var borderRadiusPicker = document.getElementById("borderRadiusPicker");
+var amountOfRows = document.getElementById("amountOfRows");
+var clear = document.getElementById("clear");
+var printButton = document.getElementById("print");
 
 var backgroundChooser = document.getElementById("background");
 var backgroundImage = document.getElementById("backgroundImage");
 
-var accentBoxes = document.querySelectorAll(".box[accent='true']");
+var editableBoxes = document.querySelectorAll("[contenteditable='true']");
 
 var color = DOMPurify.isSupported
   ? DOMPurify.sanitize(localStorage.getItem("color")) !== null &&
     DOMPurify.sanitize(localStorage.getItem("color")) !== "" &&
     DOMPurify.sanitize(localStorage.getItem("color")).length === 7
     ? DOMPurify.sanitize(localStorage.getItem("color"))
-    : "#273a13"
+    : "#000000"
   : "#000000";
-console.log(DOMPurify.sanitize(localStorage.getItem("color")));
+var borderRadius = DOMPurify.isSupported
+  ? DOMPurify.sanitize(localStorage.getItem("border-radius")) !== null &&
+    DOMPurify.sanitize(localStorage.getItem("border-radius")) !== "" &&
+    DOMPurify.sanitize(localStorage.getItem("border-radius")) >= 0 &&
+    DOMPurify.sanitize(localStorage.getItem("border-radius")) <= 33
+    ? Number(DOMPurify.sanitize(localStorage.getItem("border-radius")))
+    : 10
+  : 10;
+var rows = DOMPurify.isSupported
+  ? DOMPurify.sanitize(localStorage.getItem("rows")) !== null &&
+    DOMPurify.sanitize(localStorage.getItem("rows")) !== "" &&
+    (DOMPurify.sanitize(localStorage.getItem("rows")) == 3 ||
+      DOMPurify.sanitize(localStorage.getItem("rows")) == 4 ||
+      DOMPurify.sanitize(localStorage.getItem("rows")) == 5)
+    ? Number(DOMPurify.sanitize(localStorage.getItem("rows")))
+    : 4
+  : 4;
 
 backgroundChooser.addEventListener("change", (event) => {
   let file = event.target.files[0];
@@ -28,48 +47,106 @@ backgroundChooser.addEventListener("change", (event) => {
   }
 });
 
+editableBoxes.forEach((element) => {
+  element.oninput = function () {
+    if (DOMPurify.isSupported) {
+      if (DOMPurify.sanitize(element.innerHTML) === "") {
+        element.innerHTML = "<br>";
+      }
+    }
+  };
+});
+
 colorPicker.oninput = function () {
   if (DOMPurify.isSupported) {
     color = DOMPurify.sanitize(colorPicker.value);
-    colorDisplay.style.backgroundColor = DOMPurify.sanitize(color);
-    accentBoxes.forEach((element) => {
-      element.style.backgroundColor = DOMPurify.sanitize(color);
-    });
+    root.style.setProperty("--background-color", DOMPurify.sanitize(color));
     if (hexToHSL(DOMPurify.sanitize(color))[2] > 65) {
-      accentBoxes.forEach((element) => {
-        element.style.color = "#000000";
-      });
       root.style.setProperty("--accent-text-color", "#000000");
     } else {
-      accentBoxes.forEach((element) => {
-        element.style.color = "#ffffff";
-      });
       root.style.setProperty("--accent-text-color", "#ffffff");
     }
     localStorage.setItem("color", DOMPurify.sanitize(color));
   }
 };
 
+borderRadiusPicker.oninput = function () {
+  if (DOMPurify.isSupported) {
+    borderRadius = Number(DOMPurify.sanitize(borderRadiusPicker.value));
+    root.style.setProperty(
+      "--border-radius",
+      DOMPurify.sanitize(borderRadius + "px")
+    );
+    root.style.setProperty(
+      "--border-radius-print",
+      DOMPurify.sanitize(borderRadius / 10 + "vw")
+    );
+    localStorage.setItem("border-radius", borderRadius);
+  }
+};
+
+amountOfRows.onchange = function () {
+  if (DOMPurify.isSupported) {
+    rows = Number(DOMPurify.sanitize(amountOfRows.value));
+    localStorage.setItem("rows", Number(DOMPurify.sanitize(rows)));
+    if (Number(DOMPurify.sanitize(rows)) === 3) {
+      document.getElementById("row4").setAttribute("show", false);
+      document.getElementById("row5").setAttribute("show", false);
+    } else if (Number(DOMPurify.sanitize(rows)) === 4) {
+      document.getElementById("row4").setAttribute("show", true);
+      document.getElementById("row5").setAttribute("show", false);
+    } else {
+      document.getElementById("row4").setAttribute("show", true);
+      document.getElementById("row5").setAttribute("show", true);
+    }
+  }
+};
+
 if (DOMPurify.isSupported) {
   colorPicker.value = DOMPurify.sanitize(color);
-  colorDisplay.style.backgroundColor = DOMPurify.sanitize(color);
-  accentBoxes.forEach((element) => {
-    element.style.backgroundColor = DOMPurify.sanitize(color);
-  });
+  root.style.setProperty("--background-color", DOMPurify.sanitize(color));
   if (hexToHSL(DOMPurify.sanitize(color))[2] > 65) {
-    accentBoxes.forEach((element) => {
-      element.style.color = "#000000";
-    });
     root.style.setProperty("--accent-text-color", "#000000");
   } else {
-    accentBoxes.forEach((element) => {
-      element.style.color = "#ffffff";
-    });
     root.style.setProperty("--accent-text-color", "#ffffff");
   }
 
   backgroundImage.src = "images/background.png";
+
+  borderRadiusPicker.value = Number(DOMPurify.sanitize(borderRadius));
+  root.style.setProperty(
+    "--border-radius",
+    DOMPurify.sanitize(borderRadius + "px")
+  );
+  root.style.setProperty(
+    "--border-radius-print",
+    DOMPurify.sanitize(borderRadius / 10 + "vw")
+  );
+
+  if (Number(DOMPurify.sanitize(rows)) === 3) {
+    document.getElementById("row4").setAttribute("show", false);
+    document.getElementById("row5").setAttribute("show", false);
+  } else if (Number(DOMPurify.sanitize(rows)) === 4) {
+    document.getElementById("row4").setAttribute("show", true);
+    document.getElementById("row5").setAttribute("show", false);
+  } else if (Number(DOMPurify.sanitize(rows)) === 5) {
+    document.getElementById("row4").setAttribute("show", true);
+    document.getElementById("row5").setAttribute("show", true);
+  }
 }
+
+function setTwoSubjects(value, id) {
+  document.getElementById(id).setAttribute("two-subjects", value);
+}
+
+clear.onclick = function () {
+  localStorage.clear();
+  location.reload();
+};
+
+printButton.onclick = function () {
+  window.print();
+};
 
 function hexToHSL(H) {
   let r = 0,
